98f81ad83396e8faca5eb2fe9871758add235849
yeona <215266@jnu.ac.kr>
2024-05-11 23:47:07 +0900

    채팅 기능 삭제

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -27,7 +27,7 @@ public class MainActivity extends AppCompatActivity {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_recruit, R.id.navigation_board , R.id.navigation_safety, R.id.navigation_chat, R.id.navigation_mypage)
+                R.id.navigation_recruit, R.id.navigation_board , R.id.navigation_safety, R.id.navigation_mypage)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
--- app/src/main/java/com/example/myapplication/ui/chat/ChatViewModel.java
+++
@@ -1,7 +0,0 @@
-package com.example.myapplication.ui.chat;
-
-import androidx.lifecycle.ViewModel;
-
-public class ChatViewModel extends ViewModel {
-    // TODO: Implement the ViewModel
-}
